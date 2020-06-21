745c5f51a6ffe5758c63b73bba86495da6fef842
M1NH42 <minhazalam365@gmail.com>
2020-06-21 12:50:49 +0530

    Add code snippets

--- letmetweet.py
+++ letmetweet.py
@@ -1,6 +1,9 @@
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+import tweepy
+auth = tweepy.OAuthHandler('6JUnvO96Q8HDO3oPQ1ovu9a1y',
+                           'MoZIYs5HGzGv2lBPByI5pm2UvgnJ7UmXDNN0CcaeTh6f8ZQQTk')
+auth.set_access_token('1104351004609441794-P1sl9v874vqxsANvGJGpcZZzfJljdv',
+                      'Uq6TQG7zZxVvnkIbJrIc7Vnr6ikrknvR67ycbjBoLVhGP')
 
 api = tweepy.API(auth)
 
